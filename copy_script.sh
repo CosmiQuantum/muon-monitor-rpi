f8d68efcb29016cbde72cd28880d55024150f9af
CharlieVeihmeyer <160341608+CharlieVeihmeyer@users.noreply.github.com>
2024-06-27 15:46:34 -0500

    Update copy_script.sh

--- copy_script.sh
+++ copy_script.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-sshpass -p "muonium" rsync -ae muonium@192.168.0.112:/home/muonium/data/muon_tagging/$(date '+%Y_%m%d')/$(date '+%y%m%d')_*.csv /home/louduser/cveihmeyer/test_drop/
+sshpass -p "<>" rsync -ae muonium@192.168.0.112:/home/muonium/data/muon_tagging/$(date '+%Y_%m%d')/$(date '+%y%m%d')_*.csv /home/louduser/cveihmeyer/test_drop/
